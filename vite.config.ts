ecff382b8dac1d1c10f9c0cd0e13aff948301a16
Brainovisionsolution <135101966+Brainovisionsolution@users.noreply.github.com>
2025-04-19 15:37:43 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      input: './index.html', // Ensure this points to your main HTML file
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'lucide': ['lucide-react'],
+        },
+      },
+    },
   },
 });
