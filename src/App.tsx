eb423c42a5a974a1d5c865bd572f931d388f5cb2
Brainovisionsolution <135101966+Brainovisionsolution@users.noreply.github.com>
2025-05-06 22:04:31 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import OnlineInternship from './pages/OnlineInternship';
+import HybridInternship from './pages/HybridInternship';
 import ScrollToTop from './components/ScrollToTop';
 import WhatsAppChat from './components/WhatsAppChat';
 
@@ -13,10 +14,11 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/online" element={<OnlineInternship />} />
+        <Route path="/hybrid" element={<HybridInternship />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
 }
 
-export default App
+export default App;
